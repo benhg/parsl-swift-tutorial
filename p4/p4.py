1556a72352c51f5f9a25fdbdba692ee5b9485cb0
Benjamin Glick <glick@lclark.edu>
2017-08-30 13:52:24 -0500

    add P4, P5, and P6

---
+++ p4/p4.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from parsl import *
+import parsl
+from parsl.execution_provider.midway.slurm import Midway
+
+workers = IPyParallelExecutor(
+    engine_json_file='~/.ipython/profile_default/security/ipcontroller-engine.json')
+dfk = DataFlowKernel(workers)
+
+
+def midway_setup():
+    conf = {"site": "pool1",
+            "queue": "bigmem",
+            "maxnodes": 4,
+            "walltime": '00:04:00',
+            "controller": "10.50.181.1:50001"}
+
+    pool1 = Midway(conf)
+    pool1.scale_out(1)
+    pool1.scale_out(1)
+
+
+@App('bash', dfk)
+def setup():
+    cmd_line = "export PATH=$PWD/../app/:$PATH"
+
+# Set this example up as a bash app, which will call a command line argument
+
+
+@App('bash', dfk)
+def sort(unsorted, stdout="sorted.out", stderr="sorted.err"):
+    cmd_line = "sort {}".format(unsorted)
+
+
+if __name__ == '__main__':
+    setup()
+    unsorted = "unsorted.txt"
+    sorted = "sorted.txt"
+    sort(unsorted, stdout=sorted)

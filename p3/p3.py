b3d1ebb4ac58611fae07b05e5b605df00c0e92d3
Benjamin Glick <glick@lclark.edu>
2017-08-30 11:02:26 -0500

    Add first few swift tutorial examples in parsl

---
+++ p3/p3.py
@@ -0,0 +1,41 @@
+from parsl import *
+
+workers = ThreadPoolExecutor(max_workers=4)
+dfk = DataFlowKernel(workers)
+
+
+@App('bash', dfk)
+def setup():
+    cmd_line = "export PATH=$PWD/app/:$PATH"
+
+# Set this example up as a bash app, which will call a command line argument
+
+
+@App('bash', dfk)
+def mysim(stdout="sim.out", stderr="sim.err"):
+    cmd_line = "../app/simulate"
+
+
+@App('python', dfk)
+def start_many_sims(num_tasks=10):
+    outputs = []
+    deps = []
+    for i in range(0, num_tasks):
+        outputfile = "sim_{}".format(i)
+        a = mysim(stdout=outputfile + ".out", stderr=outputfile + ".err")
+        outputs.append(outputfile + ".out")
+        deps.append(a)
+    return outputs, deps
+
+
+@App('bash', dfk)
+def stats(deps=[], inputs=[], stdout='average.out'):
+    cmd_line = "stats {}".format(" ".join(inputs))
+
+
+if __name__ == '__main__':
+    setup()
+    results = start_many_sims()
+    inputs = results.result()[0]
+    deps = [dep.result() for dep in results.result()[1]]
+    stats(deps=deps, inputs=inputs)

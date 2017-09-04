2c2b00690d309cf5a6e387b37238b15b8e4ad66d
Benjamin Glick <glick@lclark.edu>
2017-09-04 16:05:06 -0700

    Fix inputs and outputs with regards to wrapping in futures

--- p3/p3.py
+++ p3/p3.py
@@ -12,7 +12,7 @@ def setup()-> Future:
 
 
 @App('bash', dfk)
-def mysim(stdout: str="sim.out", stder: str="sim.err")-> Future:
+def mysim(stdout: str="sim.out", stderr: str="sim.err", outputs: list=[])-> Future:
     """Call simulate on the command line"""
     cmd_line = "simulate"
 
@@ -21,17 +21,16 @@ def mysim(stdout: str="sim.out", stder: str="sim.err")-> Future:
 def start_many_sims(num_tasks: int=10)-> Future:
     """Start many concurrent simulations on the command line"""
     outputs = []
-    deps = []
     for i in range(0, num_tasks):
         outputfile = "output/sim_{}".format(i)
-        a = mysim(stdout=outputfile + ".out", stderr=outputfile + ".err")
-        outputs.append(outputfile + ".out")
-        deps.append(a)
-    return outputs, deps
+        a = mysim(stdout=outputfile + ".out", stderr=outputfile +
+                  ".err", outputs=[outputfile + ".out"])
+        outputs.append(a)
+    return outputs
 
 
 @App('bash', dfk)
-def stats(deps: list=[], inputs: list=[],
+def stats(inputs: list=[],
           stderr: str='output/average.err',
           stdout: str='output/average.out')-> Future:
     """call stats cli utility with all simulations ans inputs"""
@@ -41,12 +40,9 @@ def stats(deps: list=[], inputs: list=[],
 if __name__ == '__main__':
     # Make execution wait until after path is set
     setup().result()
-
-    results = start_many_sims()
+    results = [i[1][0].result() for i in start_many_sims().result()]
+    print(results)
     # Get filenames of simulation outputs
-    inputs = results.result()[0]
     # Get futures that the stats function will depend on.
-    # making the entire workflow wait until the next step is ready
-    deps = [dep.result() for dep in results.result()[1]]
     # pass dependencies into stats function
-    stats(deps=deps, inputs=inputs)
+    stats(inputs=results)
